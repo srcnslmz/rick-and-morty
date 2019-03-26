69ca97f4f4975c3bb270fa7dc0ae1d761eb89ae0
Ebru Gulec <ebru@ebrugulec.com>
2019-03-26 23:56:55 +0300

    Updates

---
+++ precache-manifest.2568907b8264e92b22658ac347437418.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c578be119491bc2bef3f",
+    "url": "/rick-and-morty/static/js/runtime~main.6390f2d1.js"
+  },
+  {
+    "revision": "8e1c1abbb12f939cf688",
+    "url": "/rick-and-morty/static/js/main.b3119f5a.chunk.js"
+  },
+  {
+    "revision": "6e33e311637c294f5f9d",
+    "url": "/rick-and-morty/static/js/2.2898572c.chunk.js"
+  },
+  {
+    "revision": "8e1c1abbb12f939cf688",
+    "url": "/rick-and-morty/static/css/main.6fb76652.chunk.css"
+  },
+  {
+    "revision": "07c9c6a0238cce23b5d2c53474d82785",
+    "url": "/rick-and-morty/index.html"
+  }
+];
